ca4547d33971b51a8ed8f0eaf9c6ea0d164f0831
naldal <cec7378@gmail.com>
2022-02-08 21:19:53 +0900

    Initial Commit

---
+++ GoodList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GoodList
+//
+//  Created by 송하민 on 2022/02/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
